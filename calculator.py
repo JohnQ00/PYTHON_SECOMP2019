be408814a5f4fabc35528dc82b2172cee5f02a60
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-10-02 18:36:21 -0300

    Update calculator.py

---
+++ calculator.py
@@ -0,0 +1,22 @@
+def calculator(a,op,b):
+	if op == 'sum':
+		return a + b
+	elif op == 'sub':
+		return a - b
+	elif op == 'mult':
+		return a * b
+	elif op == 'div':
+		return a / b
+	elif op == 'pow':
+		return a ** b
+	elif op == 'rad':
+		return a ** (1/b)
+	else:
+	    print('Comando invalido')
+
+def main():
+	op = input('Escolha a opção desejada (sum / sub / mult / div / pow / rad ): ')
+	a = input('Insira o primeiro valor: ')
+	b = input('Insira o segundo valor: ')
+	print(calculator(float(a),op,float(b)))
+main()
